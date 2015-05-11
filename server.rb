7746ae99d714c7f40e2ce5be4eff94a0ecd72d8f
Your Name <you@example.com>
2015-05-11 16:52:51 -0400

    xmen api

--- server.rb
+++ server.rb
@@ -5,16 +5,45 @@ db = SQLite3::Database.new "xmen.db"
 rows = db.execute <<-SQL
 create table if not exists xmen (
 	id INTEGER PRIMARY KEY,
-	name TEXT)
+	realname TEXT, alias TEXT
 );
 SQL
+
+
+
+
+
 get '/' do
 redirect('/mutants')
+end
 
 get "/mutants" do 
-	erb :index.html, 
+	mutant_list= db.execute("SELECT * FROM xmen;")
 
+	erb :index, locals: {muts: mutant_list}
+end
 
+get "/mutants/:id" do
+	id = params[:id].to_i
+	mutant= db.execute("SELECT * FROM xmen WHERE id = ?", id)
+	erb :show, locals: {mut: mutant[0]}
+end
 
+post "/mutants" do
+	db.execute("INSERT INTO xmen (realname, alias) VALUES (?,?);", params[:new_name], params[:new_alias])
+	redirect('/')
+end
 
+put "/mutants/:id" do
+	id=params[:id].to_i
+	puts params[:newname]
+	e_mutant= db.execute("UPDATE xmen SET realname= ?, alias= ? WHERE id=?;", params[:newname], params[:newalias], id)
+	redirect("/mutants/#{id}")
+end
+
+delete "/mutants/:id" do 
+	id=params[:id].to_i
+	d_mutant= db.execute("DELETE FROM xmen WHERE id=?;", id)
+	redirect('/')
+end
 #
